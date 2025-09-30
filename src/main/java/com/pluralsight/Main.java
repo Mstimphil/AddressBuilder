5cfd340080dc22bed04f5f9f950c39c75ee3d2ac
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-30 13:19:54 -0400

    initial comment

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder skills = new StringBuilder();
+        skills.append("Glover Smith\n");
+        skills.append("123 Main Street\n");
+
+    }
+}
