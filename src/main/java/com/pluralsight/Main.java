7762fe2811cc8cb4ccafdc30a08d978544c0df74
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-30 13:33:38 -0400

    skills comment

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,9 +2,26 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        // create a StringBuillder object
         StringBuilder skills = new StringBuilder();
+        // append strings to the StringBuilder object
         skills.append("Glover Smith\n");
         skills.append("123 Main Street\n");
+        skills.append("Middleton, \n");
+        skills.append("TX\n");
+        skills.append("75111 \n");
+
+        skills.append("Shipping Address: \n");
+        skills.append("456 Big Sky Blvd\n");
+        skills.append("Outer Rim, \n");
+        skills.append("TX \n");
+        skills.append("75333")
+;
+        // retrieve the underlying characters from
+        // the StringBuilder
+        String mySkills = skills.toString();
+        System.out.println(mySkills);
+
 
     }
 }
